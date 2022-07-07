5c2c8eddb70f3dc69f5028cdb8c6acbd2b3aea48
Iwuchkwu O <kizob123@gmail.com>
2022-07-07 01:52:45 +0100

    added read me instructions

--- src/api/getPosts.js
+++ src/api/getPosts.js
@@ -5,6 +5,7 @@ import {
 export let getPosts = async function(id) {
     if(id ===null)
     id=''
+    console.log(git, loc)
     const postData = await fetch(`${git}/posts/${id}` ,{
         method: 'GET',
         headers: {
